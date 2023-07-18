b48250b1df8660f4c74580a5659ff5d95f3dfe47
peter <peter.berghuis_13@hotmail.com>
2023-07-18 22:57:55 +0200

    a nice little app

---
+++ src/components/Square.jsx
@@ -0,0 +1,13 @@
+export function Square({ value, onSquareClick }) {
+
+    return (
+        <>
+            <button
+                className="square"
+                onClick={onSquareClick}
+            >
+                {value}
+            </button>
+        </>
+    )
+}
